4a89406855414a423a799c08c38d0cc991f7e746
Devrath <devrath.dev595@gmail.com>
2021-07-29 06:18:37 +0530

    x

---
+++ Applications/ApplicationOne/app/src/main/java/com/example/code/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.code
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.code.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setOnClickListener()
+    }
+
+    private fun setOnClickListener() {
+
+        binding.initiateDownloadId.setOnClickListener {
+
+        }
+
+    }
+
+
+}
