987cf3fece3e44ed5ca11671ac0f7abff411999f
Devrath <devrath.dev595@gmail.com>
2021-08-06 21:29:18 +0530

    v

--- Applications/ApplicationOne/app/src/main/java/com/example/code/custom/Constants.kt
+++ Applications/ApplicationOne/app/src/main/java/com/example/code/custom/Constants.kt
@@ -8,10 +8,6 @@ object Constants {
     const val CHANNEL_6_NAME = "Channel 6"
     const val CHANNEL_6_DESCRIPTION = "This is Channel 6"
 
-    const val DOWNLOAD_CANCEL = "DOWNLOAD_CANCEL"
-    const val DOWNLOAD_PAUSE = "DOWNLOAD_PAUSE"
-    const val DOWNLOAD_RESUME = "DOWNLOAD_RESUME"
-
     const val imageURL = "http://speedtest.ftp.otenet.gr/files/test10Mb.db"
     //const val imageURL = "https://mpl-dev-builds.s3.ap-south-1.amazonaws.com/2021-08-05/MPL_com.mpl.androidapp_debug_development_qa_1.0.134_development_qa_20210805_08_57_1000134_134_20210805_08_57_sealed_zip.apk"
     const val pauseState = "PAUSE"
@@ -19,23 +15,15 @@ object Constants {
     const val downloadingState = "Downloading"
     const val completedState = "Completed"
 
-
-
     const val FILTER_DOWNLOAD_PAUSE = "com.example.code.DOWNLOAD_PAUSE"
     const val FILTER_DOWNLOAD_RESUME = "com.example.code.DOWNLOAD_RESUME"
     const val FILTER_DOWNLOAD_CANCEL = "com.example.code.DOWNLOAD_CANCEL"
     const val FILTER_DOWNLOAD_COMPLETE = DownloadManager.ACTION_DOWNLOAD_COMPLETE
-
-
-    private const val UNIQUE_WORK_NAME = "UNIQUE_WORK_NAME"
-
-
 
     const val DOWNLOAD_STATUS_FAILED = "Failed"
     const val DOWNLOAD_STATUS_PAUSED = "Paused"
     const val DOWNLOAD_STATUS_RUNNING = "Running"
     const val DOWNLOAD_STATUS_COMPLETED = "Completed"
     const val DOWNLOAD_STATUS_PENDING = "Pending"
-
 
 }
--- Applications/ApplicationOne/app/src/main/java/com/example/code/custom/MainActivity.kt
+++ Applications/ApplicationOne/app/src/main/java/com/example/code/custom/MainActivity.kt
@@ -1,109 +1,23 @@
 package com.example.code.custom
 
-import android.annotation.SuppressLint
-import android.app.DownloadManager
-import android.content.IntentFilter
-import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
-import android.webkit.URLUtil
 import androidx.appcompat.app.AppCompatActivity
 import androidx.work.*
-import com.example.code.R
-import com.example.code.custom.Constants.FILTER_DOWNLOAD_CANCEL
-import com.example.code.custom.Constants.FILTER_DOWNLOAD_COMPLETE
-import com.example.code.custom.Constants.FILTER_DOWNLOAD_PAUSE
-import com.example.code.custom.Constants.FILTER_DOWNLOAD_RESUME
-import com.example.code.custom.Constants.downloadingState
 import com.example.code.custom.Constants.imageURL
-import com.example.code.custom.data.DownloadData.downloadedData
-import com.example.code.custom.utils.NotificationChannelApiLevel.isDownloadManagerEqualOrAbove
-import com.example.code.custom.data.DownloadModel
-import com.example.code.custom.reciever.DownloadReceiver
-import com.example.code.custom.workers.DownloadWorker
 import com.example.code.databinding.ActivityMainBinding
-import java.io.File
 import java.util.*
 
-@SuppressLint("Range")
 class MainActivity : AppCompatActivity() {
 
-    var downloadPath = ""
-
     private lateinit var binding: ActivityMainBinding
-
-    var receiver = DownloadReceiver()
-
-    val workManager = WorkManager.getInstance(this@MainActivity)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setOnClickListener()
-        setFilePath()
-    }
-
-    private fun setOnClickListener() {
-        binding.apply {
-            initiateDownloadId.setOnClickListener { downloadFile(imageURL) }
+        binding.initiateDownloadId.setOnClickListener {
+            DownloadTask(this@MainActivity,imageURL).initiateDownload()
         }
     }
-
-    private fun setFilePath() {
-        downloadPath = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).toString()
-    }
-
-    private fun downloadFile(url: String) {
-        val filename = URLUtil.guessFileName(url, null, null)
-        val file = File(downloadPath, filename)
-        var request: DownloadManager.Request? = null
-
-        request = if (isDownloadManagerEqualOrAbove()) {
-            DownloadManager.Request(Uri.parse(url)).apply {
-                setTitle(filename)
-                setDescription(getString(R.string.str_desc_downloading))
-                setNotificationVisibility(DownloadManager.Request.VISIBILITY_HIDDEN)
-                setDestinationUri(Uri.fromFile(file))
-                setRequiresCharging(false)
-                setAllowedOverMetered(true)
-                setAllowedOverRoaming(true)
-            }
-        } else {
-            DownloadManager.Request(Uri.parse(url)).apply {
-                setTitle(filename)
-                setDescription(getString(R.string.str_desc_downloading))
-                setNotificationVisibility(DownloadManager.Request.VISIBILITY_HIDDEN)
-                setDestinationUri(Uri.fromFile(file))
-                setAllowedOverMetered(true)
-                setAllowedOverRoaming(true)
-            }
-        }
-        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-        val downloadEnqueueId = downloadManager.enqueue(request)
-
-        downloadedData = DownloadModel().apply {
-            status = downloadingState
-            title = filename
-            fileSize = "0"
-            progress = "0"
-            isPaused = false
-            downloadId = downloadEnqueueId
-            filePath = ""
-        }
-
-        initWorkManager()
-    }
-
-    private fun initWorkManager() {
-        workManager.beginUniqueWork("ordering_work", ExistingWorkPolicy.REPLACE,
-                        OneTimeWorkRequestBuilder<DownloadWorker>()
-                        .addTag("DownloadWorker")
-                        .setConstraints(Constraints.Builder()
-                        .setRequiredNetworkType(NetworkType.CONNECTED).build())
-                        .build())
-                        .enqueue()
-    }
-
 
 }
